048194492cdc88eaa18494f9d00f3c638863a466
Asaad Balla <asaadballah@gmail.com>
2018-12-08 14:29:11 +0800

    Add files via upload
    done:
          1- css file
          2- layout.html
    in progress:
           3- path with one change

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from bfs2 import Queue, bfs, getPath
-from bfs2 import Queue, bfs, getPath
 from buildGraph import Graph
 
 app = Flask(__name__)
@@ -14,26 +13,22 @@ def index():
 @app.route('/', methods=['POST'])
 def getInput():
 
-    word1 = request.form['firstWord']
-    word2 = request.form['lastWord']
-
-    # if (type(word1) is 'int'  or type(word1) == int):
-    #     print(type(word1))
-    #     return render_template('resultFail.html', msg='One of the inputs is not a word')
+    word1 = (request.form['firstWord']).strip()
+    word2 = (request.form['lastWord']).strip()
 
     if ((word1 in Graph and word2 in Graph) and (len(word1) != len(word2))):
-        return render_template('resultFail.html', msg='the two words have different lengths')
+        return render_template('resultFailure.html', msg='The two words have different lengths')
+
+    elif (word1 == word2):
+        return render_template('resultFailure.html', msg='The two words are the same')
 
     elif (not (word1 in Graph and word2 in Graph)):
-        return render_template('resultFail.html', msg='One of the words is not in the list')
-
-    elif (word1 is word2):
-        return render_template('resultFail.html', msg='the two words are the same')
+        return render_template('resultFailure.html', msg='One of the words is not in the list')
 
     elif (len(word1) == len(word2)):
 
         if (word1 not in Graph or word2 not in Graph):
-            return render_template('resultFail.html', msg='One of the words is not in the list')
+            return render_template('resultFailure.html', msg='One of the words is not in the list')
 
         elif word1 in Graph:
             start = Graph[word1]
@@ -44,10 +39,10 @@ def getInput():
                 length = len(path)
                 print(path, length)
                 if (length == 2):
-                    return render_template('resultFail.html', msg='There is no path between these two words in the list')
+                    return render_template('resultFailure.html', msg='There is no path between these two words in the list')
                 elif (length != 2):
-                    return render_template("result.html", firstWord=word1, lastWord=word2, path=path, len=length)
+                    return render_template("resultSuccess.html", firstWord=word1, lastWord=word2, path=path, len=length)
 
 
 if __name__ == '__main__':
-    app.run(port=5, debug=True)
+    app.run(port=5008, debug=True)
